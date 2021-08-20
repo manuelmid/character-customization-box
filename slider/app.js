017939df4422b4b427c3144bab644a8e6c2f860a
Manuel Duran <manuelmid99@gmail.com>
2021-08-20 12:58:20 +0200

    slider box added

---
+++ slider/app.js
@@ -0,0 +1,43 @@
+const carouselSlide = document.querySelector('#carousel-slider');
+const carouselBoxs = document.querySelectorAll('#carousel-slider div')
+
+//BUTTONS
+
+const prevBtn = document.querySelector('#prvBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//COUNTER
+
+let counter = 1;
+const size = carouselBoxs[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//BUTTON LISTENERS
+
+nextBtn.addEventListener('click', ()=>{
+    if(counter >= carouselBoxs.length -1) return;
+   carouselSlide.style.transition = "transform 0.1s ease-in-out";
+   counter++;
+   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', ()=>{
+    if(counter <= 0) return;
+   carouselSlide.style.transition = "transform 0.1s ease-in-out";
+   counter--;
+   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if(carouselBoxs[counter].id === 'lastClone'){
+        carouselSlide.style.transform = "none";
+        counter = carouselBoxs.length -2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(carouselBoxs[counter].id === 'firstClone'){
+        carouselSlide.style.transform = "none";
+        counter = carouselBoxs.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
