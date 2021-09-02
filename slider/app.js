a2f3fab3297bdfb034bc6dc449d960e924085bfa
John Garcia <john.garcia@ajila.com>
2021-09-02 22:42:04 +0200

    events: added personalitation title event handler, responsive: center #box in desktop

--- slider/app.js
+++ slider/app.js
@@ -16,34 +16,41 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //BUTTON LISTENERS
 
-nextBtn.addEventListener('click', ()=>{
-    if(counter >= carouselBoxs.length -1) return;
-   carouselSlide.style.transition = "transform 0.1ms ease-in-out";
-   counter++;
-   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+nextBtn.addEventListener('click', function (ev) {
+    if (counter >= carouselBoxs.length - 1) return;
+    carouselSlide.style.transition = "transform 0.1ms ease-in-out";
+    counter++;
+    var currentSize = (-size * counter);
+    carouselSlide.style.transform = 'translateX(' + currentSize + 'px)';
+    var titleContainer = ev.target;
+    titleContainer = titleContainer.offsetParent.querySelector('#tittleText');
+    if (currentSize === -598) {
+        titleContainer.innerText = "Body";
+    } else if (currentSize === -897) {
+        titleContainer.innerText = "Legs";
+    } else if (currentSize === -1196) {
+        titleContainer.innerText = "Shoes";
+    } else {
+        titleContainer.innerText = "Face";
+    }
 });
 
-prevBtn.addEventListener('click', ()=>{
-    if(counter <= 0) return;
-   carouselSlide.style.transition = "transform 0.1ms ease-in-out";
-   counter--;
-   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+prevBtn.addEventListener('click', function () {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.1ms ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
-carouselSlide.addEventListener('transitionend', ()=>{
-    if(carouselBoxs[counter].id === 'lastClone'){
+carouselSlide.addEventListener('transitionend', function () {
+    if (carouselBoxs[counter].id === 'lastClone') {
         carouselSlide.style.transform = "none";
-        counter = carouselBoxs.length -2;
+        counter = carouselBoxs.length - 2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     };
-    if(carouselBoxs[counter].id === 'firstClone'){
+    if (carouselBoxs[counter].id === 'firstClone') {
         carouselSlide.style.transform = "none";
         counter = carouselBoxs.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     };
-
-
 });
-
-
-
